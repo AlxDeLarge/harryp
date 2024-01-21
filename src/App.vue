d97b0888383e8888e7feb6d80e120f274fac04ce
Alex <alex@laptop.com>
2024-01-21 21:36:15 +0100

    bakgrund

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+    <header>
         <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/bluu" type="text/css"/>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
