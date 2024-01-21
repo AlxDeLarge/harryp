9d01030f27028702da600d781764dc56a629ee84
Alex <alex@laptop.com>
2024-01-21 09:35:40 +0100

    css

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,4 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-body { font-size: 1em; background: #221E4E; padding: 2em; font-family: 'BluuNextBoldItalic', serif;}
-.card { border-radius: 8px; box-shadow: 0 4px 10px rgba(0,0,0,.15); -webkit-box-shadow: 0 4px 10px rgba(0,0,0,.15); -moz-box-shadow: 0 4px 10px rgba(0,0,0,.15); background-color: #F1E6BA;}
-.card-topper { border-top-right-radius: 8px; border-top-left-radius: 8px; padding: 8px 16px; font-size: 2em; font-weight: 600; text-align: center; background-color: #312823; color: #ffffff;}
-.pic { max-width: 270px; max-height: 200px; padding-bottom: 1em; width: auto; height: auto;}
-.titles {font-family: serif;}
-#test { border: 2px solid black;}
-#test td { border: 1px solid black;}
-.table-responsive { overflow-x: unset; }
 </style>
