be4ee5c875e47556fb381e3bab55fd4c414295ba
Alex <alex@laptop.com>
2024-02-04 19:55:38 +0100

    Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,39 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-<!--  <main>
-    <TheWelcome />
-</main> -->
+    <!--  <main>
+        <TheWelcome />
+        </main> -->
+        <v-container>
+            <v-row no-gutters>
+                <RouterLink to="/characters">
+                <v-col>
+                    <v-card
+                        class="mx-auto my-8 link-card"
+                        max-width="344"
+                        title="Karaktärer"
+                        prepend-icon="mdi-account"
+                        link
+                        ></v-card>
+                </v-col>
+                </RouterLink>
+                <RouterLink to="/spells">
+                <v-col>
+                    <v-card
+                        class="mx-auto my-8 link-card"
+                        max-width="344"
+                        title="Trollformler"
+                        prepend-icon="mdi-magic-staff"
+                        link
+                        ></v-card>
+                </v-col>
+                </RouterLink>
+            </v-row>
+        </v-container>
 </template>
+<style>
+.link-card {
+    background: #191b1c;
+    color: white;
+}
+</style>
